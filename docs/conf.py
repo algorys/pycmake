1c03f6d7b165144ee692af7a09d6db937bd132c3
Matthieu Estrada <ttamalfor@gmail.com>
2016-07-29 09:33:14 +0200

    Change theme for docs

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'pycmake'
+project = u'PyCMake'
 copyright = u'2016, Matthieu Estrada'
 author = u'Matthieu Estrada'
 
@@ -121,7 +121,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the

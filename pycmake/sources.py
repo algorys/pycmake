f25025e06e76cd2194de245c0beca0cfce2d5365
Matthieu Estrada <ttamalfor@gmail.com>
2016-08-31 10:18:45 +0200

    Add Sources Object and Unit Tests
    Ref #1

---
+++ pycmake/sources.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016-2016: PyCMake team, actually just Estrada Matthieu
+#
+# This file is part of PyCMake.
+#
+# PyCMake is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyCMake is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with PyCMake.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from pycmake.supported import SRC_TYPE
+
+
+class Sources(object):
+    """
+        Sources contains files and directory for a Project.
+    """
+
+    __recursive = False
+
+    def __init__(self):
+        self.src_type = None
+        self.from_proj = False
+        self.sources = []
+
+    def add(self, src_type, sources, from_proj=False):
+        """
+        Add sources with a specific type.
+
+        :param src_type: type of the sources: *DIR* or *FILE*
+        :type src_type: SRC_TYPE
+        :param from_proj: sources relative from project directory.
+        :type from_proj: bool
+        :param sources: sources to add.
+        :type sources: list
+        """
+
+        self.src_type = src_type
+        self.from_proj = from_proj
+        self.sources = sources
+
+    def make_recursive(self, recursive):
+        """
+        Make sources directory recursive.
+
+        :param recursive: recursive or not.
+        :type recursive: bool
+        """
+
+        if self.src_type == SRC_TYPE[0]:
+            self.__recursive = recursive
+        else:
+            raise ValueError('Sources Files can not be recursive.')
+
+    def is_recursive(self):
+        """
+        Check if sources are recursive are not.
+
+        """
+        return self.__recursive
---
+++ test/test_sources.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016-2016: PyCMake team, actually just Estrada Matthieu
+#
+# This file is part of PyCMake.
+#
+# PyCMake is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyCMake is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with PyCMake.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest2
+
+from pycmake.sources import *
+
+
+
+class TestSources(unittest2.TestCase):
+    """
+        This file test Sources class.
+    """
+
+    src = ['../../src/*.cpp', '../../src/*.h']
+
+    def test_add_sources(self):
+        under_test = Sources()
+
+        under_test.add(SRC_TYPE[0], TestSources.src, from_proj=True)
+
+        self.assertTrue(under_test.from_proj)
+        self.assertFalse(under_test.is_recursive())
+        self.assertEqual(
+            ['../../src/*.cpp', '../../src/*.h'],
+            under_test.sources
+        )
+
+    def test_sources_recursive(self):
+        under_test = Sources()
+
+        under_test.add(SRC_TYPE[0], TestSources.src, from_proj=False)
+        under_test.make_recursive(True)
+
+        self.assertTrue(under_test.is_recursive())
